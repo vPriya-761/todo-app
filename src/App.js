54a5c2ff70ffd61999b8240563c1c62e143001f6
Priya V <priya@aptonworks.com>
2022-04-27 17:20:54 +0530

    commit changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import TodoForm from './components/TodoForm';
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
